0bbe72ddf58614b7319a4da4b202b4dd83ec0137
ajgil <antoniojesusgil@gmail.com>
2021-09-20 07:57:08 +0200

    first commit

---
+++ src/components/stellar-wallet/stellar-wallet.tsx
@@ -0,0 +1,59 @@
+import { Component, State, Prop } from "@stencil/core";
+import { Server, ServerApi } from "stellar-sdk";
+
+import componentWillLoad from "./events/componentWillLoad";
+import render from "./events/render";
+
+import createAccount from "./methods/createAccount";
+import updateAccount from "./methods/updateAccount";
+import makePayment from "./methods/makePayment";
+import copyAddress from "./methods/copyAddress";
+import copySecret from "./methods/copySecret";
+import signOut from "./methods/signOut";
+import setPrompt from "./methods/setPrompt";
+
+import { Prompter } from "@prompt/prompt";
+
+interface StellarAccount {
+  publicKey: string;
+  keystore: string;
+  state?: ServerApi.AccountRecord;
+}
+
+interface Loading {
+  fund?: boolean;
+  pay?: boolean;
+  update?: boolean;
+}
+
+@Component({
+  tag: "stellar-wallet",
+  styleUrl: "stellar-wallet.scss",
+  shadow: true,
+})
+export class Wallet {
+  @State() account: StellarAccount;
+  @State() prompter: Prompter = { show: false };
+  @State() loading: Loading = {};
+  @State() error: any = null;
+
+  @Prop() server: Server;
+
+  // Component events
+  componentWillLoad() {}
+  render() {}
+
+  // Stellar methods
+  createAccount = createAccount;
+  updateAccount = updateAccount;
+  makePayment = makePayment;
+  copyAddress = copyAddress;
+  copySecret = copySecret;
+  signOut = signOut;
+
+  // Misc methods
+  setPrompt = setPrompt;
+}
+
+Wallet.prototype.componentWillLoad = componentWillLoad;
+Wallet.prototype.render = render;
